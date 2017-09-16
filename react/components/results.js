7e01a3f680d9ea6f4364332979e87daedd972c41
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-15 17:17:27 -0700

    prettier format results

--- react/components/results.js
+++ react/components/results.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { IconLoader } from './icons'
+import { IconLoader } from './icons';
 
 import './results.scss';
 
 const ResultsComponent = React.createClass({
   handleLoadMore() {
-    this.props.getPage(this.props.query, this.props.results.length)
+    this.props.getPage(this.props.query, this.props.results.length);
   },
   render() {
     var query = this.props.query;
@@ -15,76 +15,101 @@ const ResultsComponent = React.createClass({
     var screenshots = this.props.screenshots || [];
     //var aggregations = this.props.aggregations;
 
-    var resultDivs = results.map((d) => {
+    var resultDivs = results.map(d => {
       var block = d._source;
       var style = {};
-      var classNameString = "block-link";
-      if(block.thumb) {
-        style.backgroundImage = "url(https://gist.githubusercontent.com/" + block.userId + "/" + d._id + "/raw/" + block.thumb + "/thumbnail.png)"
-      }
-      else {
-        if(screenshots.indexOf(d._id + ".png") > -1){
-          style.backgroundImage = "url(http://christopheviau.com/block_screenshot/" + d._id + ".png)"
-          classNameString = classNameString + " " + "no-thumbnail";
+      var classNameString = 'block-link';
+      if (block.thumb) {
+        style.backgroundImage =
+          'url(https://gist.githubusercontent.com/' +
+          block.userId +
+          '/' +
+          d._id +
+          '/raw/' +
+          block.thumb +
+          '/thumbnail.png)';
+      } else {
+        if (screenshots.indexOf(d._id + '.png') > -1) {
+          style.backgroundImage =
+            'url(http://christopheviau.com/block_screenshot/' + d._id + '.png)';
+          classNameString = classNameString + ' ' + 'no-thumbnail';
         }
       }
       return (
-        <a key={"block-" + d._id} className={classNameString} href={"/" + block.userId + "/" + d._id} style={style} target="_blank">
+        <a
+          key={'block-' + d._id}
+          className={classNameString}
+          href={'/' + block.userId + '/' + d._id}
+          style={style}
+          target="_blank"
+        >
           <div className="block-description">{block.description}</div>
           <div className="block-user">@{block.userId}</div>
         </a>
-      )
-    })
+      );
+    });
 
     // If there is no text query we want to show a "latest query"
     var summary;
-    if(!query.text) {
+    if (!query.text) {
       //console.log("LATEST!")
-      var mostleast = query.sort_dir === "desc" ? "most" : "least";
-      var updatedcreated = query.sort === "updated_at" ? "updated" : "created";
+      var mostleast = query.sort_dir === 'desc' ? 'most' : 'least';
+      var updatedcreated = query.sort === 'updated_at' ? 'updated' : 'created';
       // TODO: make the mostleast and updatedcreated into dropdown menus which affect the query.
-      summary = (<span>Showing {results.length}/{totalResults} of the {mostleast} recently {updatedcreated} blocks.</span>)
+      summary = (
+        <span>
+          Showing {results.length}/{totalResults} of the {mostleast} recently{' '}
+          {updatedcreated} blocks.
+        </span>
+      );
     } else {
-      summary = (<span>Showing {results.length}/{totalResults} of the most relevant blocks.</span>)
+      summary = (
+        <span>
+          Showing {results.length}/{totalResults} of the most relevant blocks.
+        </span>
+      );
     }
     var loading;
-    if(this.props.loading) {
-      loading = (<div className=".loading"><IconLoader></IconLoader></div>)
+    if (this.props.loading) {
+      loading = (
+        <div className=".loading">
+          <IconLoader />
+        </div>
+      );
     }
 
     var loadMore;
-    if(results.length < totalResults) {
-      loadMore = <a className="load-more" onClick={this.handleLoadMore}>Load more</a>
+    if (results.length < totalResults) {
+      loadMore = (
+        <a className="load-more" onClick={this.handleLoadMore}>
+          Load more
+        </a>
+      );
     }
 
     return (
       <div id="results">
-        <div className="summary">
-          {summary}
-        </div>
-        <div className="blocks">
-          {resultDivs}
-        </div>
+        <div className="summary">{summary}</div>
+        <div className="blocks">{resultDivs}</div>
         {loading}
         {loadMore}
       </div>
-    )
+    );
   }
-})
+});
 
 const mapStateToProps = (state, ownProps) => {
-	return {
+  return {
     query: state.query,
     results: state.results,
     totalResults: state.totalResults,
     screenshots: state.screenshots,
     loading: state.loading
     //aggregations: state.aggregations
-  }
-}
-const mapActionsToProps = (dispatch) => {
-	return {
-  }
-}
+  };
+};
+const mapActionsToProps = dispatch => {
+  return {};
+};
 const Results = connect(mapStateToProps, mapActionsToProps)(ResultsComponent);
-export default Results
+export default Results;
