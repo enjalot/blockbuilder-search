a8b840b1901e7fc98bdc9ef7d2195b7eb427a7ca
Micah Stubbs <micah.stubbs@gmail.com>
2018-08-26 15:44:28 -0700

    yarn format

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 module.exports = {
-  entry: [
-      './react/main.js'
-	  ],
-    output: {
-      path: path.resolve(__dirname, 'public'),
-      filename: 'bundle.js',
-      publicPath: '/',
-    },
-    progress: true,
-    colors: true,
-    devtool: "#inline-source-map",
-    module: {
-        loaders: [{
-          test: /\.js?$/,
-			    include: path.join(__dirname, 'react'),
-          loader: "babel-loader",
-          query: {
-            presets: ['es2015', 'react', 'stage-1']
-          }
-        }, {
-          test: /\.scss$/,
-          loader: 'style!css!sass'
-        }],
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx', '.scss', '.css', '.json']
-    }
-};
+  entry: ['./react/main.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  progress: true,
+  colors: true,
+  devtool: '#inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        include: path.join(__dirname, 'react'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-1']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss', '.css', '.json']
+  }
+}
