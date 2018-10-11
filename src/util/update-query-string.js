14d92bb118f474d1a0dab55adfd37c7d3597d44c
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-10 18:25:48 -0700

    delete query param if there are no values anymore

--- src/util/update-query-string.js
+++ src/util/update-query-string.js
@@ -1,7 +1,9 @@
 export default function updateQueryString(key, value) {
   const url = new URL(window.location)
   const params = new URLSearchParams(url.search)
-  params.set(key, value)
+  if (value.length === 0) params.delete(key)
+  else params.set(key, value)
+
   url.search = params.toString()
   history.pushState({}, '', url.toString())
 }
