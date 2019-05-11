64498d44bca4f0f5cceae37690e6788c15899d29
Cameron Yick <cameron.yick@gmail.com>
2019-05-11 18:27:49 -0400

    [dependencies] Use modern style-loader and PropTypes

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["css-loader", "sass-loader"]
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
     ]
   },
